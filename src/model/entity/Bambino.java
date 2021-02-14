9ac51938e6af76b0aaefdd308fab6dcbed3e2bd6
vincenzo-esposit0 <v.esposito103@studenti.unisa.it>
2021-02-14 17:48:02 +0100

    Controlli sul bambino dei matches (metodo)

--- src/model/entity/Bambino.java
+++ src/model/entity/Bambino.java
@@ -141,15 +141,15 @@ public class Bambino implements Serializable {
 	}
 
 	public static final boolean matches(Bambino b) {
-		if(!b.getNome().matches("^[A-Za-z]{3,}$")
-			|| !b.getCognome().matches("^[A-Za-z]{3,}$")
+		if(!b.getNome().matches("^[A-Za-z ]{3,}$")
+			|| !b.getCognome().matches("^[A-Za-z ]{3,}$")
 			|| !b.getCodiceFiscale().matches("^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$")
 			|| (b.getEta()>=18)
 			|| b.getDataNascita().after(new Date())
-			|| !b.getLuogoNascita().matches("^[A-Za-z]{3,}$")
-			|| !b.getInfoEsigenzeAlimentari().matches("^[A-Za-z0-9]$")
-			|| !b.getFarmaci().matches("^[A-Za-z0-9]$")
-			|| !b.getInfoAllergie().matches("^[A-Za-z0-9]$")) {
+			|| !b.getLuogoNascita().matches("^[A-Za-z ]{3,}$")
+			|| !b.getInfoEsigenzeAlimentari().matches("^[A-Za-z 0-9]$")
+			|| !b.getFarmaci().matches("^[A-Za-z 0-9]$")
+			|| !b.getInfoAllergie().matches("^[A-Za-z 0-9]$")) {
 			
 			return false;
 			
